d8faa120b360f91bb8afe510a787dd228d716eec
Hug3O <131569471+hugo529100@users.noreply.github.com>
2025-05-15 14:07:01 +0800

    Delete plugin.js

--- plugin.js
+++
@@ -1,5 +0,0 @@
-exports.version = 1.0
-exports.apiRequired = 8.1
-exports.description = "Prevent closing dialogs by clicking the outside area. Useful on mobile to avoid accidental closes. Thanks to rejetto."
-exports.repo = "Hug3O/Prevent-dialog-close"
-exports.frontend_js = "main.js"
